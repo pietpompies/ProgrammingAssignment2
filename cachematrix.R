2c7472323c5fc543d42097549fbcb76a4db0e30c
pietpompies <eugenem65@gmail.com>
2014-04-21 16:24:07 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function will create a special matrix, which is really a list containing a function to
+##     set the value of the matrix
+##     get the value of the matrix
+##     set the value of the inverse
+##     get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
 
 }
